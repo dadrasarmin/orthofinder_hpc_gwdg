4d37575c0bba3576e228bee39cfb4ba76fa7cfac
Armin Dadras <dadrasarmin@gmail.com>
2023-09-06 14:18:23 +0200

    If there is a pre-run it took only 1 minute. So, I changed the requirements to put the job in front of the queue.

--- orthofinder_run2.sh
+++ orthofinder_run2.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 #SBATCH -p medium
 #SBATCH -C scratch
-#SBATCH -c 60
+#SBATCH -c 20
 #SBATCH -N 1
-#SBATCH --mem=90G
-#SBATCH -t 04:00:00
+#SBATCH --mem=30G
+#SBATCH -t 00:30:00
 #SBATCH -o job-%J.out
 #SBATCH --mail-type=BEGIN,END
-#SBATCH --mail-user=YOUR_EMAIL_ADDRESS@uni-goettingen.de
+#SBATCH --mail-user=armin.dadras@uni-goettingen.de
 
-python $HOME/OrthoFinder/orthofinder.py -t 50 -a 6 -y -n run_2 -ft PATH_TO_THE_PREVIOUS_RUN/raw_prot_seqs/primary_transcripts/OrthoFinder/Results_run_1 -s SpeciesTree_input.txt  &> orthofinder_run2.log
+python /usr/users/dadras/bin/OrthoFinder/orthofinder.py -t 50 -a 6 -y -n run_2 -ft /scratch/users/dadras/taja/orthofinder/raw_prot_seqs/primary_transcripts/OrthoFinder/Results_run_1 -s SpeciesTree_input.txt  &> orthofinder_run2.log
