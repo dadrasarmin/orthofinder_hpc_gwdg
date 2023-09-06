e4bf790ed794e0e61a08735e2a2050c9c3ef6013
Armin Dadras <dadrasarmin@gmail.com>
2023-09-06 13:26:32 +0200

    Update orthofinder_run2.sh

--- orthofinder_run2.sh
+++ orthofinder_run2.sh
@@ -9,4 +9,4 @@
 #SBATCH --mail-type=BEGIN,END
 #SBATCH --mail-user=YOUR_EMAIL_ADDRESS@uni-goettingen.de
 
-python $HOME/OrthoFinder/orthofinder.py -f PATH_TO_YOUR_FILES/raw_prot_seqs/primary_transcripts -S diamond -M msa -A mafft -T fasttree -t 50 -a 6 -y -n run_2 -ft PATH_TO_A_PREVIOUS_RUN/raw_prot_seqs/primary_transcripts/OrthoFinder/Results_run_1 -s SpeciesTree_input.txt  &> orthofinder_run2.log
+python $HOME/OrthoFinder/orthofinder.py -t 50 -a 6 -y -n run_2 -ft PATH_TO_THE_PREVIOUS_RUN/raw_prot_seqs/primary_transcripts/OrthoFinder/Results_run_1 -s SpeciesTree_input.txt  &> orthofinder_run2.log
